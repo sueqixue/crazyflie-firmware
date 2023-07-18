a53e374647792973f6562ab5a362f6b60f9910a7
sueqixue <xueqi923145227@gmail.com>
2023-07-18 15:29:35 -0400

    Update mycontroller

--- examples/my_controller/src/my_controller.c
+++ examples/my_controller/src/my_controller.c
@@ -26,7 +26,6 @@
  *   2 seconds.
  */
 
-
 #include <string.h>
 #include <stdint.h>
 #include <stdbool.h>
@@ -36,20 +35,29 @@
 #include "FreeRTOS.h"
 #include "task.h"
 
-#define DEBUG_MODULE "HELLOWORLD"
+// Edit the debug name to get nice debug prints
+#define DEBUG_MODULE "MYCONTROLLER"
 #include "debug.h"
 
-#include "controller.h"
-#include "controller_pid.h"
-
-
+// We still need an appMain() function, but we will not really use it. Just let it quietly sleep.
 void appMain() {
   DEBUG_PRINT("Waiting for activation ...\n");
 
   while(1) {
     vTaskDelay(M2T(2000));
+
+    // Remove the DEBUG_PRINT.
+    // DEBUG_PRINT("Hello World!\n");
   }
 }
+
+// The new controller goes here --------------------------------------------
+// Move the includes to the the top of the file if you want to
+#include "controller.h"
+
+// Call the PID controller in this example to make it possible to fly. When you implement you own controller, there is
+// no need to include the pid controller.
+#include "controller_pid.h"
 
 void controllerOutOfTreeInit() {
   // Initialize your controller data here...
